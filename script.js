3cef3ff42f2dcea361f092281d754923955eb7f0
krish20101511022608705268420 <24006ka@hvhs.school.nz>
2026-06-29 21:22:17 +0000

    Jun 29, 2026, 9:22 PM

--- script.js
+++ script.js
@@ -2,6 +2,17 @@
 ///Functions
 function startOrder() {
 let nameInput = document.getElementById("customerName").value;
+let itemInput = document.gerElementById("itemChoice").value;
+
+
+
+
+
+
+
+
+
+
 let message = "Order started:\n " + " Customer name: " + nameInput;
 document.getElementById("outputMessage").innerText = message;
 }
